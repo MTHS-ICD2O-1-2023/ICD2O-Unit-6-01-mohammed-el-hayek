f1efeffe0e1dac2035470d91275ead0d38ae0302
mohammed-el-hayek <159185058+mohammed-el-hayek@users.noreply.github.com>
2024-05-17 02:40:04 +0000

    V6-01.1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Mohammed EL-hayek All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Mohammed
+// Created on: Apr 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * Check service worker support"
+*/
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit-6-01-Mohammed-El-Hayek/sw.js", {
+    scope: "/ICS2O-Unit-6-01-Mohammed-El-Hayek/",
+  })
+}
+
+function myButtonClicked() {
+  alert("Answer will be supported soon")
+}
